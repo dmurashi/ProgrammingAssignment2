69732281f396542a528da81ed54ad10db1f7a32a
Derek Murashige <derek@dgmdomains.com>
2014-07-27 15:21:52 -0700

    R Programming week 2 assignment cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## mackeCacheMatrix is a function that contains a list of functions that can be used to 
+## makeCacheMatrix is a function that contains a list of functions that can be used to 
 ##  invert a matrix and store it in a cache
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -17,9 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setSolve = setSolve,
        getSolve = getSolve)
-  
 }
-
 
 ## cacheSolve will return the inverse of a matrix that is contained within a makeCacheMatrix 
 ##   argument that is passed in.  If the cache is not null, it will return the cache data. 
